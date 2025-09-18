5b116c7e992b55b274cf67dfa8bdb6a030f32155
Hachard Victor <31635811+VictorHachard@users.noreply.github.com>
2025-09-18 21:37:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ if __name__ == "__main__":
         # Check if today's notification has already been sent
         if not has_notification_been_sent(now.date().isoformat()):
             agenda_url = "https://hehplanning2025.umons.ac.be/Telechargements/ical/Edt_M0_Pass_Info_vers_Master_Informatique.ics?version=2025.5.6&icalsecurise=08861B133B1D1B3671E24F0A0B3CDF7F38107CD1F4F05BE68F3EB400F66270D3A53470C915AD2045ABD49481A5055CA9&param=643d5b312e2e36325d2666683d3126663d3131303030"
-            trip_duration_minutes = 45
+            trip_duration_minutes = 45 + 15  # 45 min trip + 15 min buffer
 
             try:
                 first_class, last_class = get_first_and_last_class(agenda_url, now)
